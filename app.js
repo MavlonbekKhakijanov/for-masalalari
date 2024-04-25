bcdd1a8ce397ee870470678eb930e2ba19533b2a
MavlonbekKhakijanov <khakijanovamor@gmail.com>
2024-04-25 09:46:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// 1-masala num2 ni num1 nechaga teng bo'lsa shuncha marta chiqaradi
+// let num1 = 5;
+// let num2 = 6;
+
+// if (num1 > 0) {
+//   for (let i = 0; i < num1; i++) {
+//     console.log(num2);
+//   }
+// }
+
+// 2-masala ikkita son kiritiladi va manashular orasidagi sonlar chiqariladi
+// let a = 58;
+// let b = 77;
+// if (a < b) {
+//   for (let i = a; i <= b; i++) {
+//     console.log(i);
+//   }
+// }
+
+// 3-masala kamayish tartitbida
+// let a = 58;
+// let b = 77;
+// if (a < b) {
+//   for (let i = b; i > a; i--) {
+//     console.log(i);
+//   }
+// }
+
+// 4-masala; qancha kg olishi va olgan kg shokoladi qancha summa bo'lishi
+// let chocolat = Number(prompt("1kg shokolad 18ming sum necha kg kerak"));
+// let a = 0;
+// for (let i = 0; i < chocolat; i++) {
+//   a++;
+// }
+// alert(`${(a * 18000) / 1000}.ming sum bo'ldi`);
+
+// 5-masala;
+// let chocolat = Number(prompt("1kg shokolad 18ming sum necha kg kerak"));
+// let a = chocolat;
+// for (let i = 1; i < chocolat; i++) {}
+// alert(`${(a * 18000) / 10} sum bo'ldi`);
+
+// 6-masala a dan b gacha bo'lgan sonlar yegindisi
+// let a = 1;
+// let b = 10;
+// let counter = 0;
+// if (a < b) {
+//   for (let i = 0; i <= b; i++) {
+//     counter += i;
+//   }
+// }
+// console.log(counter);
+
+// 7-masala a dan b gacha sonlar ko'paytmasi;
+// let a = 1;
+// let b = 10;
+// let counter = 1;
+// let k = 0;
+// if (a < b) {
+//   for (let i = 1; i <= b; i++) {
+//     counter *= i;
+//   }
+// }
+// console.log(counter);
+
+// 8-masala kvadiratining yegindisi
+// let a = 1;
+// let b = 10;
+// let counter = 1;
+// let k = 0;
+// if (a < b) {
+//   for (let i = 1; i <= b; i++) {
+//     counter += i ** 2;
+//   }
+// }
+// console.log(counter);
